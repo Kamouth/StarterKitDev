968cc8e7e3326611a9a981785fd0f78f7143b5ec
Camille Vaccalut <camille.vaccalut@gmail.com>
2026-08-04 19:31:03 +0200

    Add ao audio test

--- doyob-starter-kit-dev/addons/StarterKit/Zoos/AmbientOcclusionSamplerNode.gd
+++ doyob-starter-kit-dev/addons/StarterKit/Zoos/AmbientOcclusionSamplerNode.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @export var occlusion : float = 0.0
+@export var audioPlayer : AudioStreamPlayer3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,13 +11,14 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	UpdateOcclusion()
-	print(occlusion)
+	audioPlayer.max_distance = 30.0 - 9.0 * occlusion
+	# print(occlusion)
 
 func UpdateOcclusion():
 	occlusion = AmbientOcclusionSampler.compute_occlusion(
 	get_world_3d(),
 	global_position,
-	Vector3.UP,
+	Vector3.MODEL_FRONT,
 	8.0,
 	128
 )
